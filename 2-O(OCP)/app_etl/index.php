744940b76582575156c5d38144011e32f25061d6
gezerramos2 <gezerramo@gmail.com>
2022-07-04 00:08:26 -0300

    init OCP

---
+++ 2-O(OCP)/app_etl/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__."/vendor/autoload.php";
+
+use src\Leitor;
+
+$leitor = new Leitor();
+
+$leitor->setDiretorio(__DIR__.'/files');
+
+$leitor->setArquivo('dados.csv');
+
+echo "<pre>";
+print_r($leitor->lerArquivo());
+echo "</pre>";
